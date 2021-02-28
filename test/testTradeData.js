b959fdb6446a7ca1b6a4ec070b55fc77e09785be
Jason Tigas <20135683+jiveyTO@users.noreply.github.com>
2021-02-28 08:10:50 -0500

    make return calc easier to read and fix a bug

--- test/testTradeData.js
+++ test/testTradeData.js
@@ -54,6 +54,20 @@ const tradesArray = [
     quantity: 1,
     createdAt: new Date(2021, 1, 19),
     updatedAt: new Date(2021, 1, 19)
+  },
+  {
+    id: 5,
+    guild: '1234567890',
+    trader: 'johnSmith',
+    ticker: 'TSLA',
+    type: 'Put',
+    action: 'STO',
+    expiry: new Date(2021, 1, 26),
+    strike: 400,
+    price: 1.00,
+    quantity: 4,
+    createdAt: new Date(2021, 1, 22),
+    updatedAt: new Date(2021, 1, 22)
   }
 ]
 
@@ -79,3 +93,24 @@ const pricesArray = [
 ]
 
 exports.prices = pricesArray
+
+const historicalPrices = {
+  'TSLA|2021-02-26': {
+    date: '2021-02-26',
+    open: 700,
+    high: 706.7,
+    low: 659.51,
+    close: 675.5,
+    volume: 41089173
+  },
+  'FB|2021-02-26': {
+    date: '2021-02-26',
+    open: 256.47,
+    high: 265.55,
+    low: 256.47,
+    close: 257.62,
+    volume: 26619517
+  }
+}
+
+exports.historical = historicalPrices
